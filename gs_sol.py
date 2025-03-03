959831a885e74c3603798e8d7df7b71b240ccbe6
tado-mi <tadouchiha@gmail.com>
2025-03-02 21:44:01 -0800

    q2 + minor fixes

--- gs_sol.py
+++ gs_sol.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import sys
 import xarray as xr
 
@@ -29,6 +31,7 @@ def print_info(ds):
 
     print("\n----------")
 
+# q1
 def get_max_hmax_in_location(ds, lon, lat):
     # get all the available time values
     time_vals = ds.coords["time"].values
@@ -36,8 +39,42 @@ def get_max_hmax_in_location(ds, lon, lat):
     hmax_vals = [ds["hmax"].sel(longitude=lon, latitude=lat, time=t).values for t in time_vals]
     return max(hmax_vals)
 
+def get_max_hmax_string(ds, lon, lat):
+    return f"the highest wave height at coordinates ({lon}, {lat}) is {get_max_hmax_in_location(ds, lon, lat)}."
+
+#q2
+def get_points(ds):
+    lon_vals = ds.coords["longitude"].values
+    lat_vals = ds.coords["latitude"].values
+    full_grid = [[float(lon), float(lat)] for lon in lon_vals for lat in lat_vals]
+    return np.array(full_grid)
+
+def create_ui(ds):
+    points = get_points(ds)
+    x, y = points[:, 0], points[:, 1]
+    fig, ax = plt.subplots()
+    # enable picker
+    sc = ax.scatter(x, y, c='blue', picker=True)
+    # text 
+    txt = ax.text(0.5, 1.05, "click on a point", transform=ax.transAxes, ha="center")
+
+    def on_click(event):
+        # get the index of the selected point
+        ind = event.ind
+        if len(ind) > 0:
+            lon, lat = points[ind[0]]
+            # update the text
+            txt.set_text(get_max_hmax_string(ds, lon, lat))  # Update text
+            # redraw
+            fig.canvas.draw()
+
+    # connect onclick
+    fig.canvas.mpl_connect('pick_event', on_click)
+    plt.show()
+
+
 # filepath
-filenpath = "waves_2019-01-01.nc" if len(sys.argv) < 2 else sys.argv[1]
+filepath = "waves_2019-01-01.nc" if len(sys.argv) < 2 else sys.argv[1]
 ds = xr.open_dataset(filepath)
 
 # uncomment to see info about the input
@@ -47,7 +84,12 @@ ds = xr.open_dataset(filepath)
 print("\nq1:")
 lon = 0.000
 lat = 0.000
-print(f"the highest wave height at coordinates ({lon}, {lat}) is {get_max_hmax_in_location(ds, lon, lat)}.")
+print(get_max_hmax_string(ds, lon, lat))
+
+# q2
+print("\nq2:")
+print("loading ui...")
+create_ui(ds)
 
 # close ds
 ds.close()
